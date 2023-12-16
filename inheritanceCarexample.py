d27efa8f8bf549968162b95c9caed61e3f46522d
anmolbava <anmolbava96@gmail.com>
2023-12-16 13:08:15 +0000

    inheritance 2

--- inheritanceCarexample.py
+++ inheritanceCarexample.py
@@ -9,8 +9,15 @@ class Car():
         print("Speed decreasing")
 
 class harrier(Car):
+    def racemode(self):
+        print("race mode is on")
+
+class verna(Car):
     pass
 
 h1 = harrier(4,6,250)
+v1 = verna(4,5,350)
 print(h1.gears)
 h1.speedUp()
+h1.racemode()
+v1.racemode()
